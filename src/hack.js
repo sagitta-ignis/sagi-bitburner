acec19b8bf3c8bc846cc7db455202d874f9acd42
Sagitta <sagitta.ignis@gmail.com>
2022-09-25 14:39:39 +0200

    feat(hack): add standalone/deployable scripts for weaken,grow,hack

---
+++ src/hack.js
@@ -0,0 +1,25 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	const args = ns.flags([["help", false]]);
+	let target = ns.args[0];
+    let untilDate = ns.args[1];
+    if (args.help || !target) {
+        ns.tprint("This script runs hack() on a given target.");
+        ns.tprint(`Usage: run ${ns.getScriptName()} target [iso-datetime]`);
+        ns.tprint("Example:");
+        ns.tprint(`> run ${ns.getScriptName()} n00dles "${new Date(Date.now() + 1000*60*5).toISOString()}"`);
+        return;
+    }
+    let until = 0;
+    if (untilDate) until = Math.max(Date.parse(untilDate) - Date.now(), 0);
+    if (until) await ns.sleep(until);
+	await ns.hack(target);
+}
+
+/**
+ *  @param {AutocompleteData} data
+ *  @param {string[]} args
+ */
+ export function autocomplete(data, args) {
+    return data.servers;
+}
